41166fa53c280a0e175116afbc2257d59d745b97
Jagriti <Jagriti2351999@gmail.com>
2022-07-07 15:20:10 +0530

    Updates

---
+++ precache-manifest.4e7db65bc866a4c3f9f93e97d39001cf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "05634f2af3e7cebebf7b",
+    "url": "/React-js/static/css/main.61d414b0.chunk.css"
+  },
+  {
+    "revision": "05634f2af3e7cebebf7b",
+    "url": "/React-js/static/js/main.05634f2a.chunk.js"
+  },
+  {
+    "revision": "03ae7ea834bbe7645a20",
+    "url": "/React-js/static/js/1.03ae7ea8.chunk.js"
+  },
+  {
+    "revision": "2b811ff04c7e6ec04e74",
+    "url": "/React-js/static/js/2.2b811ff0.chunk.js"
+  },
+  {
+    "revision": "9ff06069ec557ad86ac1",
+    "url": "/React-js/static/js/runtime~main.9ff06069.js"
+  },
+  {
+    "revision": "706cfc88dfd58b112712ba8f607c09f5",
+    "url": "/React-js/index.html"
+  }
+];
